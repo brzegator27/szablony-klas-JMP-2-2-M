2c88e05f3343bfcf6d301180c5953d9ec349ffc7
brzegator27 <symfiz@gmail.com>
2015-05-16 13:52:59 +0200

    Added comments in aghMatrix.cpp and special functions in aghMatrix class resposible for allocation and deallocation of memory for matrix.

--- szablony klas JMP 2 2 M/aghMatrix.cpp
+++ szablony klas JMP 2 2 M/aghMatrix.cpp
@@ -17,46 +17,70 @@ using namespace std;
 template class aghMatrix<int>;	/** Written in order to have templates of class' functions in .cpp file
 								*	More on: https://isocpp.org/wiki/faq/templates#separate-template-class-defn-from-decl
 								*/
-									
+				
 
+template <class T>
+void aghMatrix<T>::allocateMem(int m, int n)
+{
+	this->pointer = new T*[m];	/// Creation of table for pointers to tables with rows' values
+
+	for (int i = 0; i < m; i++)
+		this->pointer[i] = new T[n];	/// Allocation of memory for tables representing each row
+
+	this->m = m;	/// Setting filed in aghMatrix object in order to know how many rows we have created
+	this->n = m;	/// Setting filed in aghMatrix object in order to know how many columns we have created
+
+	for (int i = 0; i < m; i++)
+		for (int j = 0; j < n; j++)
+			this->pointer[i][j] = 0;	/// By Default values are set to 0
+}
+
+template <class T>
+void aghMatrix<T>::deallocateMem()
+{
+	for (int i = 1; i < this->m; i++)
+		delete[] this->pointer[i];	/// We deallocate memory of tables in which are values from rows
+
+	delete[] this->pointer;		/// Deletion of table with pointers to tables with rows' values
+
+	this->m = 0;	/// Setting number of rows in object's matrix to 0 because now we don't have any
+	this->n = 0;	/// Setting number of columns in object's matrix to 0 because now we don't have any
+}
+	
 template <class T>
 aghMatrix<T>::aghMatrix()
 {
-	this->pointer = nullptr;
+	this->pointer = nullptr;	/// Setting pointer to nullptr because there is no matrix 
+	this->m = 0;	/// Setting number of rows in object's matrix to 0 because now we don't have any
+	this->n = 0;	/// Setting number of columns in object's matrix to 0 because now we don't have any
 }
 
 template <class T>
 aghMatrix<T>::aghMatrix(int m, int n)
 {
-	this->pointer = new T*[m];
-
-	for (int i = 0; i < m; i++)
-		this->pointer[i] = new T[n];
-
-	this->m = m;
-	this->n = m;
+	allocateMem(m, n);	/// Allocatioin of memory for matrix
 }
 
 template <class T>
 aghMatrix<T>::~aghMatrix()
 {
-	for (int i = 1; i < this->m; i++)
-		delete[] this->pointer[m];
-
-	delete[] this->pointer;
+	deallocateMem();	/// Aeallocation of memory from matrix
 }
 
 template <class T>
 void aghMatrix<T>::setItem(int m, int n, T newValue)
 {
 	if (m > this->m || n > this->n || m < 0 || n < 0)
-		throw aghException(0, "Index out of range", __FILE__, __LINE__);
+		throw aghException(0, "Index out of range", __FILE__, __LINE__);	/// If somebody would like to set value, which is out of range of matrix, then we throw exception
 
-	this->pointer[m][n] = newValue;
+	this->pointer[m][n] = newValue;	/// Setting value of particular element of matrix
 }
 
 template <class T>
 T aghMatrix<T>::getItem(int m, int n)
 {
-	return this->pointer[m][n];
+	if (m > this->m || n > this->n || m < 0 || n < 0)
+		throw aghException(0, "Index out of range", __FILE__, __LINE__);	/// If somebody would like to get value, which is out of range of matrix, then we throw exception
+
+	return this->pointer[m][n];	/// Returning value of particular element of matrix
 }
--- szablony klas JMP 2 2 M/aghMatrix.h
+++ szablony klas JMP 2 2 M/aghMatrix.h
@@ -25,6 +25,15 @@ class aghMatrix
 {
 	T** pointer;	///< pointer to table containing respective fields of matrix
 	int m, n;		///< two integers with matrix size
+
+	/// \brief function allocating memory for tables representing matrix
+	///
+	/// \param m - number of rows
+	/// \param n - number of columns
+	void allocateMem(int m, int n);
+
+	/// \brief function deallocating memory from tables representing matrix
+	void deallocateMem();
 public:
 	/// \brief default constructor
 	aghMatrix();
@@ -41,8 +50,22 @@ public:
 	///
 	/// \param m - number of row
 	/// \param n - number of column
-	/// \param newValue - new value which we are going to set for particular element
+	/// \param newValue - new value which we are going to set for partic
 	void setItem(int m, int n, T newValue);
+
+	/// \brief function setting value of elements of matrix
+	///
+	/// \param m - number of row
+	/// \param n - number of column
+	/// \param newValue - new value which we are going to set for partic
+	void setItems(T* newValue);
+
+	/// \brief function setting value of element of matrix
+	///
+	/// \param m - number of row
+	/// \param n - number of column
+	/// \param newValue - new value which we are going to set for partic
+	void setItems(int m, int n, T newValue);
 
 	/// \brief function getting value of element of matrix
 	///
