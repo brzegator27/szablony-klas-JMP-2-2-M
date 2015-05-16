8e8730918224946a144d5f4101ec27178d43e2ab
brzegator27 <symfiz@gmail.com>
2015-05-16 10:43:36 +0200

    Added comments in aghMatrix in Doxygen's convention.

--- szablony klas JMP 2 2 M/aghMatrix.cpp
+++ szablony klas JMP 2 2 M/aghMatrix.cpp
@@ -1,9 +1,23 @@
+/**
+* \file aghMatrix.cpp
+* \author Jakub Brzegowski
+* \date 16.05.2015
+* \version 1.0
+* \Header file with class' functions from class aghMatrix
+*/
+// -------------------------------------------------------------------------
+
+
 #include "aghMatrix.h"
 #include <iostream>
 
 using namespace std;
 
-template class aghMatrix < int > ;
+
+template class aghMatrix<int>;	/** Written in order to have templates of class' functions in .cpp file
+								*	More on: https://isocpp.org/wiki/faq/templates#separate-template-class-defn-from-decl
+								*/
+									
 
 template <class T>
 aghMatrix<T>::aghMatrix()
--- szablony klas JMP 2 2 M/aghMatrix.h
+++ szablony klas JMP 2 2 M/aghMatrix.h
@@ -1,20 +1,58 @@
+/**
+* \file aghMatrix.h
+* \author Jakub Brzegowski
+* \date 16.05.2015
+* \version 1.0
+* \Header file with class aghMatrix
+*/
+// -------------------------------------------------------------------------
+
 #include "aghException.h"
 
-#pragma once
+#ifndef AGH_MATRIX
+#define AGH_MATRIX
+// -------------------------------------------------------------------------
+
+/**
+* \class aghMatrix
+* \author Jakub Brzegowski
+* \date 16.05.2015
+* \Class which represent matrix
+*/
+
 template <class T>
 class aghMatrix
 {
-	T** pointer;
-	int m, n;
+	T** pointer;	///< pointer to table containing respective fields of matrix
+	int m, n;		///< two integers with matrix size
 public:
+	/// \brief default constructor
 	aghMatrix();
-	aghMatrix(int, int);
+	/// \brief two-parameter constructor
+	///
+	/// \param m - number of rows
+	/// \param n - number of columns
+	aghMatrix(int m, int n);
 
+	/// \brief default destructor
 	~aghMatrix();
 
+	/// \brief function setting value of element of matrix
+	///
+	/// \param m - number of row
+	/// \param n - number of column
+	/// \param newValue - new value which we are going to set for particular element
 	void setItem(int m, int n, T newValue);
+
+	/// \brief function getting value of element of matrix
+	///
+	/// \param m - number of row
+	/// \param n - number of column
 	T getItem(int m, int n);
 };
+
+// -------------------------------------------------------------------------
+#endif
 
 
 
